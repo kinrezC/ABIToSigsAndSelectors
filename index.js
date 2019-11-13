946eec606709eefda83aed2adcc9926780f575be
Kinrezc <matt.czernik@gmail.com>
2019-11-13 17:56:47 -0500

    fix imports and naming clarity

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-var abi = require('./abi');
-var { pick } = require('lodash');
-var Web3 = require('web3');
-var fs = require('fs');
+const abi = require("./abi");
+const { pick } = require("lodash");
+const Web3 = require("web3");
+const fs = require("fs");
 
 const web3 = new Web3();
 
-const selectors = JSON.stringify(
+const functionSelectors = JSON.stringify(
   abi
-    .filter(obj => obj.type == 'function')
-    .map(obj => pick(obj, ['name', 'inputs']))
+    .filter(obj => obj.type == "function")
+    .map(obj => pick(obj, ["name", "inputs"]))
     .map(obj => {
       const name = obj.name;
       const argTypes = obj.inputs.map(params => params.type);
@@ -19,18 +19,18 @@ const selectors = JSON.stringify(
       const bytes = web3.utils.soliditySha3(sig).substring(0, 10);
       return {
         signature: sig,
-        selector: bytes,
+        selector: bytes
       };
     })
     .reduce((parsedAbi, obj) => {
       parsedAbi[obj.selector] = {
-        signature: obj.signature,
+        signature: obj.signature
       };
       return parsedAbi;
-    }, {}),
+    }, {})
 );
 
-fs.writeFile('./functions.json', selectors, err => {
+fs.writeFile("./functionSelectors.json", functionSelectors, err => {
   if (err) {
     console.log(err);
   }
@@ -38,8 +38,8 @@ fs.writeFile('./functions.json', selectors, err => {
 
 const eventSelectors = JSON.stringify(
   abi
-    .filter(obj => obj.type == 'event')
-    .map(obj => pick(obj, ['name', 'inputs']))
+    .filter(obj => obj.type == "event")
+    .map(obj => pick(obj, ["name", "inputs"]))
     .map(obj => {
       const name = obj.name;
       const argTypes = obj.inputs.map(params => params.type);
@@ -50,20 +50,18 @@ const eventSelectors = JSON.stringify(
       const bytes = web3.utils.soliditySha3(sig).substring(0, 10);
       return {
         signature: sig,
-        selector: bytes,
+        selector: bytes
       };
     })
     .reduce((eventSelectors, obj) => {
       eventSelectors[obj.selector] = {
-        signature: obj.signature,
+        signature: obj.signature
       };
       return eventSelectors;
-    }, {}),
+    }, {})
 );
 
-console.log(eventSelectors);
-
-fs.writeFile('./events.json', eventSelectors, err => {
+fs.writeFile("./events.json", eventSelectors, err => {
   if (err) {
     console.log(err);
   }
